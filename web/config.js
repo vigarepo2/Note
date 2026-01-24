44526af3f3242a9ca95abb5b8d5f2486ae5acbc4
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2026-01-24 18:50:23 +0530

    Delete web directory

--- web/config.js
+++
@@ -1,8 +0,0 @@
-const AppConfig = {
-    domain: "opplex.org:8080",
-    username: "aalokgh",
-    password: "pass54321",
-    getStreamUrl: function(id) {
-        return `http://${this.domain}/${this.username}/${this.password}/${id}`;
-    }
-};
